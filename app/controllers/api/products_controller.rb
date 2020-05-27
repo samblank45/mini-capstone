22ea54d77228418180e93abd07ceb9f17d811ffe
sam <sblan45@gmail.com>
2020-05-27 12:55:27 -0400

    adds initial work

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,8 @@
 class Api::ProductsController < ApplicationController
+
+def store_page
+  
+  render 'store.json.jb'
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: "blue bottle coffee", price: 5, image_url: "https://image.cnbcfm.com/api/v1/image/105989971-1561586961365bbcoffeebeanbags.jpg?v=1561587056". descrption: "delicious blue coffee straight from the source")
