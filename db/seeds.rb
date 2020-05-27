ce0f6b38aac0a9e6128859c3ceff4315e257a55e
sam <sblan45@gmail.com>
2020-05-27 13:53:45 -0400

    adds ceramic mug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 # product = Product.new(name: "blue bottle coffee", price: 5, image_url: "https://image.cnbcfm.com/api/v1/image/105989971-1561586961365bbcoffeebeanbags.jpg?v=1561587056", description: "delicious blue coffee straight from the source")
 # product.save
 
-product = Product.new(name: "starbucks coffee", price: 10, image_url: "https://cdn.vox-cdn.com/thumbor/vajM6ILbU-rXaek527VXPxB4aDc=/0x0:6000x4000/920x613/filters:focal(1463x1566:2423x2526):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/65808525/shutterstock_1391647442.0.jpg", description: "sugary coffe drink")
-product.save
+# product = Product.new(name: "starbucks coffee", price: 10, image_url: "https://cdn.vox-cdn.com/thumbor/vajM6ILbU-rXaek527VXPxB4aDc=/0x0:6000x4000/920x613/filters:focal(1463x1566:2423x2526):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/65808525/shutterstock_1391647442.0.jpg", description: "sugary coffe drink")
+# product.save
+
+# product = Product.new(name: "ceramic mug", price: 12, image_url: "https://images.squarespace-cdn.com/content/v1/5563ff5ae4b02c9d26fbff64/1527733520829-I1YD3OYSD3J5RRHPRPVY/ke17ZwdGBToddI8pDm48kO13kjY42Rx8PDuIzbsUGx97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdW2N0leaE2nInJg7KpfqMS1mKwPiDa5VuS53xofCarKLCwLgRJzBxPM8v-1l46jLA/IMG_0916.JPG?format=1000w", description: "decorative mug for coffee")
+# product.save
