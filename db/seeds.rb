12b3b2eba70086bf06491921dce588ad7364ea34
sam <sblan45@gmail.com>
2020-05-27 13:48:36 -0400

    adds starbucks coffee

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # product = Product.new(name: "blue bottle coffee", price: 5, image_url: "https://image.cnbcfm.com/api/v1/image/105989971-1561586961365bbcoffeebeanbags.jpg?v=1561587056", description: "delicious blue coffee straight from the source")
 # product.save
 
-product = Product.new(name: "starbucks coffee", price: 5, image_url: "https://image.cnbcfm.com/api/v1/image/105989971-1561586961365bbcoffeebeanbags.jpg?v=1561587056", description: "delicious blue coffee straight from the source")
+product = Product.new(name: "starbucks coffee", price: 10, image_url: "https://cdn.vox-cdn.com/thumbor/vajM6ILbU-rXaek527VXPxB4aDc=/0x0:6000x4000/920x613/filters:focal(1463x1566:2423x2526):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/65808525/shutterstock_1391647442.0.jpg", description: "sugary coffe drink")
 product.save
