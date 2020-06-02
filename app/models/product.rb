1b77fd651a14ac98981295fb4d114706295cef45
sam <sblan45@gmail.com>
2020-06-02 17:00:38 -0400

    adds models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,23 @@
 class Product < ApplicationRecord
+
+
+  def is_discounted?
+    discount = ""
+    if price > 10
+      true
+    else
+      false
+    end
+  end
+
+  def tax
+    tax = price * 0.09
+    tax 
+  end
+
+  def total
+    total_price = tax + price
+    total_price
+  end
+
 end
